49abb0c7bbb0926e1224a2ddbec3c240b7d15d44
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2021-11-29 13:19:40 +0200

    Revert "transferring functions and constants from index"
    This reverts commit 7a72ca5b3a399b8aadbd0d16d31805b688459644.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
 import "/css/style.css"
-import { DOM } from "./dom.js"
-import { singIn, singUp, hideForm } from "./singInFunctions"
-import { click } from "./constants"
+import {DOM} from "./dom.js"
 
-DOM.singInButton.addEventListener(click, singIn);
-DOM.singUpButton.addEventListener(click, singUp);
+DOM.singInButton.addEventListener('click', singIn);
+DOM.singUpButton.addEventListener('click', singUp);
+
+function hideForm() {
+    loginForm.classList.add('none');
+}
+function singIn(e) {
+    e.preventDefault();
+    console.log('test');
+}
+function singUp(e) {
+    e.preventDefault();
+}
