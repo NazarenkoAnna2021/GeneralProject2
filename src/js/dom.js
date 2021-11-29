d8e2546c48c2ebaed0fde83547b18f74a717fdea
lesssnic <antoxxxa.ua@gmail.com>
2021-11-29 11:59:07 +0200

    login-form created

---
+++ src/js/dom.js
@@ -0,0 +1,11 @@
+export const DOM = {
+    singInLogin: document.querySelector('#singin input[data="login"]'),
+    singInPassword: document.querySelector('#singin input[data="password"]'),
+    singInButton: document.querySelector('#singin button'),
+    singUpFirstName: document.querySelector('#singup input[data="first_name"]'),
+    singUpLastName: document.querySelector('#singup input[data="last_name"]'),
+    singUpLogin: document.querySelector('#singup input[data="login"]'),
+    singUpPassword: document.querySelector('#singup input[data="password"]'),
+    singUpButton: document.querySelector('#singup button'),
+    loginForm: document.querySelector('.container-login')
+}
