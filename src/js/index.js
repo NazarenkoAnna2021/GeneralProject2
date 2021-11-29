7a72ca5b3a399b8aadbd0d16d31805b688459644
NazarenkoAnna2021 <93328568+NazarenkoAnna2021@users.noreply.github.com>
2021-11-29 13:17:23 +0200

    transferring functions and constants from index

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,7 @@
 import "/css/style.css"
-import {DOM} from "./dom.js"
+import { DOM } from "./dom.js"
+import { singIn, singUp, hideForm } from "./singInFunctions"
+import { click } from "./constants"
 
-DOM.singInButton.addEventListener('click', singIn);
-DOM.singUpButton.addEventListener('click', singUp);
-
-function hideForm() {
-    loginForm.classList.add('none');
-}
-function singIn(e) {
-    e.preventDefault();
-    console.log('test');
-}
-function singUp(e) {
-    e.preventDefault();
-}
+DOM.singInButton.addEventListener(click, singIn);
+DOM.singUpButton.addEventListener(click, singUp);
