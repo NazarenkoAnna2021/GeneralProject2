24b4d49ab7929b20b771aa9b4aed2eb3ef114458
lesssnic <antoxxxa.ua@gmail.com>
2021-12-09 01:30:55 +0200

    create double ranges

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import "/css/style.css"
+import "/css/filter.css"
 import { DOM } from "./dom.js"
 import { singIn, singUp } from "./singInFunctions"
 import { click } from "./constants"
 import "./img"
 import { preview } from "./preview"
+import "./filter.js"
 
 DOM.loginImg.addEventListener(click, preview);
 DOM.singInButton.addEventListener(click, singIn);
