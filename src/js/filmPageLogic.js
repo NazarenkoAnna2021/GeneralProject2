a344ea2ae81e152fd60f0506ae674ffc2f9cadc4
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-10 23:04:00 +0200

    fix styles, start fix logic for page

--- src/js/filmPageLogic.js
+++ src/js/filmPageLogic.js
@@ -23,23 +23,14 @@ function appendFilmInfoToDOM(data) {
 	const templateFilmHtml = document.querySelector('#main-container').innerHTML
 		.replace("{{titleFilm}}", data.title)
 		.replace("{{img}}", URL.imagePosterLink.concat(data.backdrop_path))
-		.replace("{{rate}}", data.movie_rate === null ? 'Rating is not defined' : `Rate: ${data.movie_rate}`)
+		.replace("{{rate}}", data.movie_rate === null ? '0' : data.movie_rate)
 		.replace("{{filmDate}}", correctReleaseDate(data.release_date))
 		.replace("{{country}}", correctReleaseDate(data.original_language.toUpperCase()))
 		.replace("{{about}}", data.overview)
 		.replace("{{tagline}}", data.tagline)
-		// .replace("{{ganre}}", bbb(data.genre_ids))
+		.replace("{{ganre}}", bbb(data.genre_ids))
 
 	const filmElem = htmlToElement(templateFilmHtml);
 	const mainContainer = document.querySelector('.main-info');
 	mainContainer.appendChild(filmElem);
 }
-
-// ============= DONT REMOVE FUNCTION =================
-//===============================================
-// async function getGenres() {
-// 	const requestGanres = await fetch(`${URL.URL}/genres`)
-// 	const response = await requestGanres.json();
-// 	return response.genres;
-// }
-//===============================================
