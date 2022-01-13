d2568bb95b8ad953ca6b8cb68a93f528f1365517
VitVin <93376524+VitVin@users.noreply.github.com>
2022-01-13 18:20:12 +0300

    CloseFilmPage

--- src/js/filmPage.js
+++ src/js/filmPage.js
@@ -1,5 +1,9 @@
 import "/css/filter.css"
 import "/css/style.css"
-import { getCurrentFilmInfo } from './filmPageLogic';
+import {closePage, getCurrentFilmInfo} from './filmPageLogic';
+import {DOM_PAGE} from "./pageDom";
+import {constants} from "./constants";
 
 getCurrentFilmInfo();
+
+DOM_PAGE.loginBtn.addEventListener(constants.click, closePage)
--- src/js/pageDom.js
+++ src/js/pageDom.js
@@ -2,5 +2,6 @@ export const DOM_PAGE = {
     templateInfo: document.querySelector('#main-container').innerHTML,
     mainInfo: document.querySelector('.main-info'),
     templateReview: document.querySelector('#reviews-content').innerHTML,
-    reviewArea: document.querySelector('.reviews__text')
+    reviewArea: document.querySelector('.reviews__text'),
+    loginBtn: document.querySelector('.login')
 }
