a59c350d346eeeef9fa3c9b70ef163cfa209e043
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-13 12:38:02 +0200

    delete img

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,30 +1,42 @@
 import { DOM } from "./dom.js";
 import { hideShowElement } from "./visibility";
-import { cleanHTML, drawCards, renderPagination } from "./gallery";
-import { URL } from "./constants";
-import { constants, pathmames, filtersParams } from "./constants";
-import axios from "axios";
+import { renderCards, cleanHTML, setCurrentPage } from "./gallery";
+import { constants, filtersParams } from "./constants";
 import { DoubleRange } from "./classes"
 
 const doubleRangeYear = new DoubleRange(constants.year, constants.yearGap);
 const doubleRangeBudget = new DoubleRange(constants.budget, constants.budgetGap);
 const doubleRangeRating = new DoubleRange(constants.rating, constants.ratingGap);
+export const params = {
+    adult: false,
+    title: DOM.headerInput.value,
+    genre: DOM.genresSelect.value,
+    budget_min: doubleRangeBudget.getValue().budget_min,
+    budget_max: doubleRangeBudget.getValue().budget_max,
+    language: DOM.countrySelect.value,
+    popularity_min: Math.round(doubleRangeRating.getValue().rating_min),
+    popularity_max: Math.round(doubleRangeRating.getValue().rating_max),
+    release_date_first: `${doubleRangeYear.getValue().year_min}-01-01`,
+    release_date_last: `${doubleRangeYear.getValue().year_max}-12-31`,
+    status: DOM.checkReleased.value
+}
 
 export function setGalleryByFilters() {
-    const params = {
-        adult: false,
-        title: DOM.headerInput.value,
-        genre: DOM.genresSelect.value,
-        budget_min: doubleRangeBudget.getValue().budget_min,
-        budget_max: doubleRangeBudget.getValue().budget_max,
-        language: DOM.countrySelect.value,
-        popularity_min: Math.round(doubleRangeRating.getValue().rating_min),
-        popularity_max: Math.round(doubleRangeRating.getValue().rating_max),
-        release_date_first: `${doubleRangeYear.getValue().year_min}-01-01`,
-        release_date_last: `${doubleRangeYear.getValue().year_max}-12-31`,
-        status: DOM.checkReleased.value
-    }
-    sendData(params);
+    params.adult = false;
+    params.title = DOM.headerInput.value;
+    params.genre = DOM.genresSelect.value;
+    params.budget_min = doubleRangeBudget.getValue().budget_min;
+    params.budget_max = doubleRangeBudget.getValue().budget_max;
+    params.language = DOM.countrySelect.value;
+    params.popularity_min = Math.round(doubleRangeRating.getValue().rating_min);
+    params.popularity_max = Math.round(doubleRangeRating.getValue().rating_max);
+    params.release_date_first = `${doubleRangeYear.getValue().year_min}-01-01`;
+    params.release_date_last = `${doubleRangeYear.getValue().year_max}-12-31`;
+    params.status = DOM.checkReleased.value;
+    setCurrentPage(0);
+    renderCards();
+    cleanHTML();
+    openFilters();
 }
 
 export function resetFilters() {
@@ -41,11 +53,11 @@ export function resetFilters() {
 
 export function openFilters() { hideShowElement(DOM.filtersForm) };
 
-async function sendData(params) {
-    const response = await axios.get(URL.URL.concat(pathmames.movies),{
-            headers: { 'Authorization': localStorage.getItem(constants.token) },
-            params: params
-        });
-    cleanHTML()
-    await drawCards(response.data.data.data);
-}
+// async function sendData(params) {
+//     const response = await axios.get(URL.URL.concat(pathmames.movies),{
+//             headers: { 'Authorization': localStorage.getItem(constants.token) },
+//             params: params
+//         });
+//     cleanHTML()
+//     await drawCards(response.data.data.data);
+// }
--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,24 +1,23 @@
 import { DOM } from "./dom.js";
 import { URL } from "./constants";
 import axios from "axios";
+import { params } from "./filter"
 
-//let current = 1;
-let films = {};
 let currentPage = 0;
 
 async function getResponseMoviePage(setOfParams) {
-	const res = await axios.get(`${URL.URL}/movies`, { 
+	const res = await axios.get(`${URL.URL}/movies`, {
 		headers: { 'Authorization': localStorage.getItem('token') },
-		params: {page: ++currentPage}
+		params: setOfParams
 	});
-	let resData = res.data.data.data
-	console.log(resData)
-	return resData;
+	return res.data.data.data
 }
 
 export async function renderCards() {
-	films = await getResponseMoviePage(++currentPage);
+	params.page = ++currentPage;
+	const films = await getResponseMoviePage(params);
 	drawCards(films);
+	console.log(params, films);
 }
 
 export function drawCards(cards) {
@@ -29,19 +28,6 @@ export function drawCards(cards) {
 
 export function cleanHTML() {
 	DOM.filmsArea.innerHTML = '';
-}
-
-function check(current, lastPage) {
-	if (current === lastPage) {
-		DOM.paginationBtnNext.classList.add("disabled");
-	} else {
-		DOM.paginationBtnNext.classList.remove("disabled");
-	}
-	if (current === 1) {
-		DOM.paginationBtnPrev.classList.add("disabled");
-	} else {
-		DOM.paginationBtnPrev.classList.remove("disabled");
-	}
 }
 
 function createCards({ id, poster_path, title, popularity }) {
@@ -58,3 +44,5 @@ export function htmlToElement(html) {
 	template.innerHTML = html;
 	return template.content.firstChild;
 }
+
+export function setCurrentPage(newCurrentPage) { currentPage = newCurrentPage }
