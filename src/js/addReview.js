a39038f06f29edaa70672127632e840c9149232f
NazarenkoAnna2021.com <93328568+NazarenkoAnna2021@users.noreply.github.com>
2022-01-14 09:27:47 +0200

    remake button

--- src/js/addReview.js
+++ src/js/addReview.js
@@ -1,27 +1,28 @@
-import {pathmames, URL} from "./constants";
+import { pathmames, URL } from "./constants";
 import { hideShowElement } from "./visibility";
 import axios from "axios";
 import { DOM_PAGE } from "./pageDom";
 
 async function getResponseMoviePage(id, content) {
     try {
-        const response = await axios.post(`${URL.URL}${pathmames.review}`, { movie_id: id, content: content },
-            {headers: { 'Authorization': localStorage.getItem('token')},
-                });
+        const response = await axios.post(`${URL.URL}${pathmames.review}`,
+            { movie_id: id, content: content },
+            {
+                headers: { 'Authorization': localStorage.getItem('token') },
+            });
     }
     catch (error) {
-       console.dir(error.response.data.error);
+        console.dir(error.response.data.error);
     }
 }
 
 export async function addReview(id, content) {
     await getResponseMoviePage(id, content);
-        changeVisibilityEdding();
-        location.reload()
+    changeVisibilityEdding();
+    location.reload()
 }
 
-export function changeVisibilityEdding(){
-    DOM_PAGE.reviewsButton.style.pointerEvents = 'none';
+export function changeVisibilityEdding() {
     hideShowElement(DOM_PAGE.reviewsAdd);
     hideShowElement(DOM_PAGE.reviewArea);
 }
